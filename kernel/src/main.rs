b76b705b4d7b462b8c97962588933d2034c059ac
thatmagicalcat <thatmagicalcat@hotmail.com>
2026-03-14 10:45:37 +0530

    some random changes

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -12,19 +12,50 @@ unsafe extern "C" {
 
 use core::{arch::asm, fmt::Write, ptr};
 
+use multiboot2 as mb2;
+
 use vga_buffer::{Buffer, Color, Writer};
 
 mod interrupts;
 mod macros;
+mod memory;
 mod vga_buffer;
 mod volatile;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn kernel_main(multiboot_info_addr: u32) -> ! {
-    println!("Hello, World!");
     interrupts::init();
+    println!("Hello, World!");
 
-    unsafe { *(0xDEADBEEF as *mut u32) = 42 };
+    let boot_info = unsafe {
+        mb2::BootInformation::load(multiboot_info_addr as *const mb2::BootInformationHeader)
+    }
+    .expect("Failed to load multiboot info");
+
+    let memory_map_tag = boot_info
+        .memory_map_tag()
+        .expect("Memory map tag not found in multiboot info");
+
+    println!("Memory areas:");
+    for area in memory_map_tag.memory_areas() {
+        println!(
+            "  - start: {:#010x}, end: {:#010x}, size: {} KB, type: {:?}",
+            area.start_address(),
+            area.end_address(),
+            (area.end_address() - area.start_address()) / 1024,
+            area.typ()
+        );
+    }
+
+    let mut frame_allocator = memory::BitmapFrameAllocator::new(&boot_info);
+    let frame = frame_allocator.allocate_frame().unwrap();
+    let ptr = frame.get_ptr();
+
+    println!("Total frames: {}", frame_allocator.total_frames);
+
+    unsafe { *ptr = 42 };
+    println!("Wrote value {} to address {:p}", unsafe { *ptr }, ptr);
+    frame_allocator.deallocate_frame(frame);
 
     #[allow(clippy::empty_loop)]
     loop {}
